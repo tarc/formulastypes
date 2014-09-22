84b999ff80ba1065c4396e54101ffe063eabac66
Tarcísio Genaro Rodrigues <tarcisio.genaro@gmail.com>
2014-09-22 11:52:19 -0300

    Fun with move constructor.

--- main.cpp
+++ main.cpp
@@ -2,15 +2,21 @@
 
 #include "fac-tmp.hpp"
 #include "fibo-tmp.hpp"
+
+#ifndef NOVARIADIC
 #include "list-tmp.hpp"
+#endif
 
 using namespace std;
 
 int main()
 {
 	cout << fact<10>::val << endl;
-	cout << fibo<256>::val << endl;
+	cout << fibo<10>::val << endl;
+
+#ifndef NOVARIADIC
 	cout << count<int, char, long>::val << endl;
+#endif
 
 	return 0;
 }
